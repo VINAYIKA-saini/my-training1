82a5487bb95eea5a5dd64f39b3c2bafa0ceb75a6
vinayika saini <vinayikasaini015@.com>
2022-08-30 17:41:08 +0530

    changing into auth-1

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -7,16 +7,6 @@ const authenticate =function (req,res,next){
     next()
 }
 
-const authorization= async function(req,res,next){
-    userid=req.params.userId
-    authorized_id=req.decodedToken["userId"]
-    if(userid==authorized_id){
-    next()
-    }else{
-      return res.status(200).send({status:false,msg:"Un-authorized_id"})
-    }
-}
+
 
 module.exports.authenticate=authenticate
-module.exports.authorization=authorization
-
